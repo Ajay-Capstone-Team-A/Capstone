e4775122feb7d95848215835095e154a9a1a6d0a
rnpeters <rnpetersen@ucdavis.edu>
2022-12-28 08:24:53 -0800

    Removed commented out code previously added

--- ECommerce.API/ECommerce.Models/Product.cs
+++ ECommerce.API/ECommerce.Models/Product.cs
@@ -12,12 +12,6 @@ namespace ECommerce.Models
     public class Product
     {
         [Key]
-        /*public int id { get; set; }
-        public string name { get; set; }
-        public int quantity { get; set; }
-        public decimal price { get; set; }
-        public string description { get; set; }
-        public string image { get; set; }*/
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public int ProductQuantity { get; set; }
@@ -35,15 +29,5 @@ namespace ECommerce.Models
             this.ProductDescription = description;
             this.ProductImage = image;
         }
-
-        /*public Product(int id, string name, int quantity, decimal price, string description, string image)
-        {
-            this.id = id;
-            this.name = name;
-            this.quantity = quantity;
-            this.price = price;
-            this.description = description;
-            this.image = image;
-        }*/
     }
 }
--- ECommerce.API/ECommerce.Models/User.cs
+++ ECommerce.API/ECommerce.Models/User.cs
@@ -8,11 +8,6 @@ namespace ECommerce.Models
     public class User
     {
         [Key]
-        /*public int id { get; set; }
-        public string firstName { get; set; }
-        public string lastName { get; set; }
-        public string email { get; set; }
-        public string password { get; set; }*/
         public int UserId { get; set; }
         public string UserFirstName { get; set; }
         public string UserLastName { get; set; }
@@ -37,21 +32,5 @@ namespace ECommerce.Models
             this.UserPassword = UserPassword;
 
         }
-
-        /*public User(int id, string firstName, string lastName, string email, string password)
-        {
-            this.id = id;
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.email = email;
-            this.password = password;
-        }
-        public User(string firstName, string lastName, string email, string password)
-        {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.email = email;
-            this.password = password;
-        }*/
     }
 }
