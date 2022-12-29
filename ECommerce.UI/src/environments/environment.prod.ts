a08f8e55033d27ef1895df9befdc7c07e5100fdf
calrenq <71615884+calrenq@users.noreply.github.com>
2022-12-29 14:28:27 -0800

    Update environment.prod.ts

--- ECommerce.UI/src/environments/environment.prod.ts
+++ ECommerce.UI/src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   baseUrl: "https://ajayacapstoneapi.azurewebsites.net", //"https://localhost:7078",
   headers: {
     'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': 'http://localhost:4200',
+    'Access-Control-Allow-Origin': 'https://icy-wave-01c980310.2.azurestaticapps.net/',
   },
 };
