cdf2ccc77028836dd8e6523a9b4aa995346add6f
calrenq <71615884+calrenq@users.noreply.github.com>
2022-12-29 12:14:28 -0800

    Update environment.prod.ts
    Attempting to fix build issues with deployment

--- ECommerce.UI/src/environments/environment.prod.ts
+++ ECommerce.UI/src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  withCredentials: true,
+  baseUrl: "https://ajayacapstoneapi.azurewebsites.net", //"https://localhost:7078",
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': 'http://localhost:4200',
+  },
 };
