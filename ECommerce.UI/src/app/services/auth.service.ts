91dd61ba2c618ddc20eea2ea123e9f0cf5c69149
rnpeters <rnpetersen@ucdavis.edu>
2022-12-30 13:20:32 -0800

    Added Remove Items From Cart Feature

--- ECommerce.UI/src/app/services/auth.service.ts
+++ ECommerce.UI/src/app/services/auth.service.ts
@@ -7,7 +7,7 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class AuthService {
-
+  
   authUrl: string = `${environment.baseUrl}/auth`;
   loggedIn: boolean = false;
 
