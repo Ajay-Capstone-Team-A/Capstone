8e55b1b00189a263c2c7003023c435c391569efa
rnpeters <rnpetersen@ucdavis.edu>
2023-01-05 12:44:52 -0800

    Fixing test cases

--- ECommerce.UI/src/app/components/register/register.component.spec.ts
+++ ECommerce.UI/src/app/components/register/register.component.spec.ts
@@ -3,18 +3,21 @@ import { Router } from '@angular/router';
 import { throwError } from 'rxjs';
 import { defer } from 'rxjs/internal/observable/defer';
 import { Product } from 'src/app/models/product';
+import { User } from 'src/app/models/user';
 import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 import { RegisterComponent } from './register.component';
 
 describe('RegisterComponent', () => {
   let spyRouter: jasmine.SpyObj<Router>;
   let spyAuth: jasmine.SpyObj<AuthService>;
+  let spyService: jasmine.SpyObj<UserService>;
   let component: RegisterComponent;
     
   
   beforeEach(async () => {
-    component = new RegisterComponent(spyAuth,spyRouter);
+    component = new RegisterComponent(spyAuth,spyService,spyRouter);
   });
   
   it('should create', () => {
@@ -25,20 +28,24 @@ describe('RegisterComponent', () => {
 describe('RegisterComponent methods', () => {
   let spyRouter: jasmine.SpyObj<Router>;
   let spyAuth: jasmine.SpyObj<AuthService>;
+  let spyService: jasmine.SpyObj<UserService>;
   let component: RegisterComponent;
     
   
   beforeEach(async () => {
     spyAuth = jasmine.createSpyObj(["register"]);
     spyRouter = jasmine.createSpyObj(["navigate"]);
+    spyService = jasmine.createSpyObj(["checkEmail","checkPassword"]);
     spyOn(window.console, "log");
-    component = new RegisterComponent(spyAuth,spyRouter);
+    component = new RegisterComponent(spyAuth,spyService,spyRouter);
   });
   
   it('should call onSubmit', () => {
-    var product = new Product(1,"bob",2,"bob",1,"b");
+    var user = new User(1,"b","b","b","b");
     let quantity = 1;
-    spyAuth.register.and.returnValue(defer(()=>Promise.resolve({product,quantity})));
+    spyService.checkEmail.and.returnValue(defer(()=>Promise.resolve(true)));
+    spyService.checkPassword.and.returnValue(defer(()=>Promise.resolve(true)));
+    spyAuth.register.and.returnValue(defer(()=>Promise.resolve({user,quantity})));
     spyRouter.navigate.and.callFake;
     component.onSubmit();
     expect(window.console.log).toHaveBeenCalled;
@@ -47,6 +54,9 @@ describe('RegisterComponent methods', () => {
   it('should call onSubmit, error', () => {
     var product = new Product(1,"bob",2,"bob",1,"b");
     let quantity = 1;
+    spyService.checkEmail.and.returnValue(defer(()=>Promise.resolve(true)));
+    spyService.checkPassword.and.returnValue(defer(()=>Promise.resolve(true)));
+   
     spyAuth.register.and.returnValue(throwError({status:404}));
     spyRouter.navigate.and.callFake;
     component.onSubmit();
