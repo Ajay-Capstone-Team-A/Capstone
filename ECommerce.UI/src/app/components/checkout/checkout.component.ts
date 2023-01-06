a3c3e2a950bec721a734d35b22285e6fe5182446
Calvin Tan <calvintan607@gmail.com>
2023-01-06 14:00:25 -0800

    input validation finished for all but street address and expiry on checkout page

--- ECommerce.UI/src/app/components/checkout/checkout.component.ts
+++ ECommerce.UI/src/app/components/checkout/checkout.component.ts
@@ -20,59 +20,113 @@ export class CheckoutComponent implements OnInit {
   finalProducts: {id: number, quantity: number}[] = []; 
 
   checkoutForm = new UntypedFormGroup({
-    fname: new UntypedFormControl('', Validators.required),
-    lname: new UntypedFormControl('', Validators.required),
-    cardName: new UntypedFormControl('', Validators.required),
-    detail: new UntypedFormControl('', Validators.required),
-    addOne: new UntypedFormControl('', Validators.required),
-    addTwo: new UntypedFormControl(''),
-    city: new UntypedFormControl('', Validators.required),
-    state: new UntypedFormControl('', Validators.required),
-    zipCode: new UntypedFormControl('', Validators.required),
-    country: new UntypedFormControl('', Validators.required)
+    cardName: new UntypedFormControl('', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]),
+    cardNumber: new UntypedFormControl('', [Validators.required, Validators.pattern("^[0-9]{16}$")]),
+    expiry: new UntypedFormControl('', [Validators.required]),
+    cvv: new UntypedFormControl('', [Validators.required,Validators.pattern("^[0-9]{3}$")]),
+    address: new UntypedFormControl('', [Validators.required]),
+    city: new UntypedFormControl('', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]),
+    state: new UntypedFormControl('', [Validators.required, Validators.pattern('^[a-zA-Z ]*$')]),
+    zipCode: new UntypedFormControl('', [Validators.required, Validators.pattern("^[0-9]{5}$")]),
   });
 
-  constructor(private productService: ProductService, private router: Router) { }
+  constructor(private productService: ProductService,private router: Router) { }
 
   ngOnInit(): void {
-    this.productService.getCart().subscribe(
-      (cart) => {
-        this.products = cart.products;
-        this.products.forEach(
-          (element) => this.cartProducts.push(element.product)
-        );
-        this.totalPrice = cart.totalPrice;
-      }
-    );
-  }
+      this.productService.getCart().subscribe(
+        (cart) => {
+          this.products = cart.products;
+          this.products.forEach(
+            (element) => this.cartProducts.push(element.product)
+          );
+          this.totalPrice = cart.totalPrice;
+        }
+      );
+    }
+
 
   onSubmit(): void {
-    this.products.forEach(
-      (element) => {
-        const id = element.product.productId;
-        const quantity = element.quantity
-        this.finalProducts.push({id, quantity})
-      } 
-    );
-
-    if(this.finalProducts.length > 0) {
-      this.productService.purchase(this.finalProducts).subscribe(
-        (resp) => console.log(resp),
-        (err) => console.log(err),
-        () => {
-          let cart = {
-            cartCount: 0,
-            products: [],
-            totalPrice: 0.00
-          };
-          this.productService.setCart(cart);
-          this.router.navigate(['/home']);
-        } 
+    if (!this.checkoutForm.invalid) {
+      this.products.forEach(
+        (element) => {
+          const id = element.product.productId;
+          const quantity = element.quantity
+          this.finalProducts.push({ id, quantity })
+        }
       );
 
+      if (this.finalProducts.length > 0) {
+        this.productService.purchase(this.finalProducts).subscribe(
+          (resp) => console.log(resp),
+          (err) => console.log(err),
+          () => {
+            let cart = {
+              cartCount: 0,
+              products: [],
+              totalPrice: 0.00
+            };
+            this.productService.setCart(cart);
+            this.router.navigate(['/home']);
+          }
+        );
+
+      } else {
+        this.router.navigate(['/home']);
+      }
     } else {
-      this.router.navigate(['/home']);
+      console.log("checkout form is invalid");
     }
+ 
   }
 
+
+
+  get cardName() {
+    return this.checkoutForm.get('cardName');
+  }
+
+  get cardNumber() {
+    return this.checkoutForm.get('cardNumber');
+  }
+
+  get expiry() {
+    return this.checkoutForm.get('expiry');
+  }
+
+  get cvv() {
+    return this.checkoutForm.get('cvv');
+  }
+
+  get address() {
+    return this.checkoutForm.get('address');
+  }
+
+  get state() {
+    return this.checkoutForm.get('state');
+  }
+
+  get city() {
+    return this.checkoutForm.get('city');
+  }
+
+  get zipCode() {
+    return this.checkoutForm.get('zipCode');
+  }
+
+  /*
+  validateExpiry(input: string) {
+  // ensure basic format is correct
+    if (input.match(/^(0[1-9]|1[0-2])\/?([0-9]{4})$/)) {
+    const { 0: month, 1: year } = input.split("/");
+
+    // get midnight of first day of the next month
+    const expiry = new Date(2000 + year, month);
+    console.log("HWERE");
+    const current = new Date();
+
+    return expiry.getTime() > current.getTime();
+
+  } else return false;
+}
+  */
 }
